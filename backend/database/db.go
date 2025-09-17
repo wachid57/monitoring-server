27f5a1ed5949487dee263004e4d727fc1feb7104
mas wachid <admin@wcloud.my.id>
2025-09-17 07:05:10 +0000

    feat: Enhance database initialization by loading environment variables from multiple locations and adding support for dynamic database port

--- backend/database/db.go
+++ backend/database/db.go
@@ -5,12 +5,27 @@ import (
 	"gorm.io/gorm"
 	"os"
 	"mini-npm-backend/model"
+	"github.com/joho/godotenv"
 )
 
 var DB *gorm.DB
 
 func InitDB() error {
-	dsn := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASS") + "@tcp(" + os.Getenv("DB_HOST") + ":3306)/" + os.Getenv("DB_NAME") + "?parseTime=true"
+	// Try to load .env from several locations
+	_ = godotenv.Load("backend/.env")
+	_ = godotenv.Load("../.env")
+	_ = godotenv.Load(".env")
+
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	if dbPort == "" {
+		dbPort = "3306"
+	}
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := dbUser + ":" + dbPass + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?parseTime=true"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return err
